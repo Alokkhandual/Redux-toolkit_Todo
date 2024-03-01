03dc588bd4fa7190b9449ea2be237578a93ee021
AlokKhandual <alokkhandual45@gmail.com>
2024-03-01 19:22:55 +0530

    using react-reduxtoolkit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import AddTodo from './components/Addtodo'
+import Todos from './components/Todos'
+
+
+function App() {
+  
+  return (
+    <>
+      <h1>Redux toolkit</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
